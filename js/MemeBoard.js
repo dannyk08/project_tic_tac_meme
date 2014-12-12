d97330f7d060a5e486960f74d7228ac811e851ed
dromero08 <romerodaniel.k90@gmail.com>
2014-12-11 18:14:35 -0800

    This is the fully functional TTT game

--- js/MemeBoard.js
+++ js/MemeBoard.js
@@ -1,31 +1,26 @@
 angular
 	.module('TicTacApp')
-	.factory('MemeBoard', MemeBoard);
+	.factory('TicTacBoard', MemeBoard);
 
 function MemeBoard() {
 
-	var TILE_STATES = ['unselected-tile', 'selected-tile'];
+	self = this;
 
-	var MemeBoard = function( numTiles ) {
-		this.numTiles = numTiles;
-		this.tiles = new Array( numTiles );
-
-		this.toggleTile = toggleTile;
-		this.getTileState = getTileState;
-
-		function toggleTile( num ) {
-			this.tiles[num] = (this.tiles[num] + 1) % TILE_STATES.length;
-		}
-
-		function getTileState( num ) {
-			return TILE_STATES[this.tiles[num]];
-		}
+	function makeTableArray(){
 		
-		for (var i=0; i<this.tiles.length; i++) {
-			this.tiles[i] = 0;
-		}
-	};
-
-	return MemeBoard;
-
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- js/TicTacController.js
+++ js/TicTacController.js
@@ -6,13 +6,88 @@ angular
 
 // $inject > $firsbase inside the controller brackets and the function
 
-TicTacController.$inject = ['MemeBoard'];
+// TicTacController.$inject = ['TicTacBoard'];
 
-function TicTacController(MemeBoard){
-	
-	this.gameName = "TicTacMeme";
-	this.activeBoard = new MemeBoard( 3 );
+	// var self = this;
 
+	function TicTacController(){
+
+		var self = this;
+
+		self.gameBoard = new Array(9);
+
+		self.playerMove = playerMove;
+
+		self.checkWin = checkWin;
+
+		self.turn = 1;
+
+
+
+// clickable tiles 
+// check whose turn it is 
+
+// set value to X or O
+// change player turn variable 
+// check for win
+		function playerMove($index){
+			if(self.gameBoard[$index] === undefined){
+				if (self.turn === 1) {
+					self.gameBoard[$index] = "X";
+					console.log($index);
+
+					self.turn = 2;
+					
+				}
+				else {
+					self.gameBoard[$index] = "O";
+					console.log($index);
+					self.turn = 1;
+					
+				}	
+
+				checkWin(console.log(self.gameBoard));			
+			}
+
+			else {
+				// alert("You can't click this!"); 
+			}
+		}
+
+		function checkWin($index){
+				
+				if ((self.gameBoard[0] === "X" && self.gameBoard[1] === "X" && self.gameBoard[2] === "X" ) ||
+				 (self.gameBoard[3] === "X" && self.gameBoard[4] === "X" && self.gameBoard[5] === "X" ) ||
+				 (self.gameBoard[6] === "X" && self.gameBoard[7] === "X" && self.gameBoard[8] === "X" ) ||
+				 (self.gameBoard[0] === "X" && self.gameBoard[3] === "X" && self.gameBoard[6] === "X" ) ||
+				 (self.gameBoard[1] === "X" && self.gameBoard[4] === "X" && self.gameBoard[7] === "X" ) ||
+				 (self.gameBoard[2] === "X" && self.gameBoard[5] === "X" && self.gameBoard[8] === "X" ) ||
+				 (self.gameBoard[0] === "X" && self.gameBoard[4] === "X" && self.gameBoard[8] === "X" ) ||
+				 (self.gameBoard[6] === "X" && self.gameBoard[4] === "X" && self.gameBoard[2] === "X" )){
+					console.log("You WON!!!");
+				}
+				else if ((self.gameBoard[0] === "O" && self.gameBoard[1] === "O" && self.gameBoard[2] === "O" ) ||
+				 (self.gameBoard[3] === "O" && self.gameBoard[4] === "O" && self.gameBoard[5] === "O" ) ||
+				 (self.gameBoard[6] === "O" && self.gameBoard[7] === "O" && self.gameBoard[8] === "O" ) ||
+				 (self.gameBoard[0] === "O" && self.gameBoard[3] === "O" && self.gameBoard[6] === "O" ) ||
+				 (self.gameBoard[1] === "O" && self.gameBoard[4] === "O" && self.gameBoard[7] === "O" ) ||
+				 (self.gameBoard[2] === "O" && self.gameBoard[5] === "O" && self.gameBoard[8] === "O" ) ||
+				 (self.gameBoard[0] === "O" && self.gameBoard[4] === "O" && self.gameBoard[8] === "O" ) ||
+				 (self.gameBoard[6] === "O" && self.gameBoard[4] === "O" && self.gameBoard[2] === "O" )){
+					console.log("You WON!!!");
+				}
+			
+				// else if (self.gameBoard === ("O"||"X")){
+				// 	console.log("There's been a draw.");
+				// }
+		} 
+
+
+		// self.playingBoard = new TicTacBoard( 9 );
+		self.gameName = "Tic Tac Meme!";
+		self.gameVersion = "1.0" ;
+		self.player1 = player1;
+		self.player2 = player2;
 	
 	// function getMeme() {
 	// 	var whatever = new Firebase("https://dromero08app.firebaseio.com/TicTacMeme");
